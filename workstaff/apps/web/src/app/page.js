6bb1b10f6310e94efff000f17e8ffdee4652dec0
Sergi <ssegarragarcia@gmail.com>
2025-08-26 12:00:34 +0000

    solucionado frontend en demo y detalles en form

--- workstaff/apps/web/src/app/page.js
+++ workstaff/apps/web/src/app/page.js
@@ -1,31 +1,19 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 export default function Home() {
-  const [animate, setAnimate] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    setAnimate(true);
-    const timer = setTimeout(() => router.push("/login"), 4000);
-    return () => clearTimeout(timer);
+  setTimeout(() => router.push("/login"), 4000);
   }, [router]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-16 px-8 sm:px-20">
-      <div
-        className={`transition-all duration-700 ease-out transform text-center -mt-50
-      ${
-        animate
-          ? "opacity-100 scale-110 -translate-y-2"
-          : "opacity-0 scale-75 translate-y-10"
-      }
-      animate-bounce-slow
-    `}
-      >
+      <div className={`animate-fade-in-up -mt-30`}>
         <Image
           src="/logo.png"
           alt="logo workstaff"
