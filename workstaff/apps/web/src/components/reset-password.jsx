cf7cbbb559a0f8debc231760bbf9429940e52f23
Sergi <ssegarragarcia@gmail.com>
2025-09-01 13:29:06 +0000

    dependencias

--- workstaff/apps/web/src/components/reset-password.jsx
+++ workstaff/apps/web/src/components/reset-password.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "../../../../lib/supabaseClient";
+import { supabase } from '../lib/supabaseClient'
 import Image from "next/image";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
